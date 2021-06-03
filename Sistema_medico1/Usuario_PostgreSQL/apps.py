6adbdd78474c267a627be1dc88a18deb88c5c67d
teamingsoft01 <teamingsoft01@gmail.com>
2021-06-03 02:56:23 -0500

    fallo

--- Sistema_medico1/Usuario_PostgreSQL/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UsuarioPostgresqlConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Usuario_PostgreSQL'
--- Sistema_medico1/Usuario_PostgreSQL/views.py
+++
@@ -1,5 +0,0 @@
-from django.http.request import HttpRequest
-from django.shortcuts import render
-
-# Create your views here.
-
